8ba7e0ce9dfdfd1a915cdff303e125317bb3e146
BurhanWajih <161490629+BurhanWajih@users.noreply.github.com>
2025-05-15 09:13:54 +0500

    Delete routes directory

--- routes/players.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getTopPlayers } = require("../controllers/playerController");
-
-router.get("/top", getTopPlayers);
-
-module.exports = router;
